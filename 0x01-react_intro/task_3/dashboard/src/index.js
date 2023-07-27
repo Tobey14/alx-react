a7a6ee4ec3c621166ab99c2e1d2ca0e7df608614
tobechukwu onuigbo <tobechukwu.onuigbo@sterling.ng>
2023-07-27 03:04:10 +0100

    Alx react

---
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,14 @@
+mport React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Notifications } from "./Notifications";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
